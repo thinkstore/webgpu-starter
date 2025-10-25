a42408c7c7293acbe92050edaa560e0d681784ad
Edward Hunter <thebytefarmer@gmail.com>
2025-10-25 19:04:40 +0300

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
 import "./style.css";
-import { WebGPU_LoadingImages_Renderer } from "./WebGPU_LoadingImages_Renderer.ts";
-import { WebGPU_Texture_Renderer } from "./WebGPU_Texture_Renderer.ts";
-// import { CellRenderer } from "./CellRenderer.ts";
-// import { Renderer } from "./Renderer.ts";
-// import { WebGPU_Uniforms_Renderer } from "./WebGPU_Uniforms_Renderer.ts";
-// import { WebGPU_StorageBuffers_Renderer } from "./WebGPU_StorageBuffers_Renderer.ts";
+import { WebGPU_ImportingImages_Renderer } from "./WebGPU_ImportingImages.ts";
 
 const canvas = document.getElementById("gpu-canvas") as HTMLCanvasElement;
 
-// const renderer = await CellRenderer.newInstance(canvas);
-// const renderer = await Renderer.newInstance(canvas);
-// const renderer = await WebGPU_Uniforms_Renderer.newInstance(canvas);
-const renderer = await WebGPU_LoadingImages_Renderer.newInstance(canvas);
+const renderer = await WebGPU_ImportingImages_Renderer.newInstance(canvas);
 renderer.startRenderLoop();
